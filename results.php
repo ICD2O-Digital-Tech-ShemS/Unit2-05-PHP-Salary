ee790b05e2971b3e5f444609ce58d56320d75519
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-03 15:08:35 +0000

    This is my updated version

--- results.php
+++ results.php
@@ -4,5 +4,8 @@
 
 	$pay = $hoursWorked * $hourlyRate
 ?>
+<?php echo
 <h3>Results:</h3>
-Your pay is <?php echo "$pay" ?>
+Your hoursWorked is . $hoursWorked. <br> 
+Your hourlyRate is . $hourlyRate. <br> 
+Your pay is . $pay . "." ?>
